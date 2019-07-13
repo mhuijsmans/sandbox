b73e866582f5337a14422f5cbc32188bb814f1da
310160231 <310160231@NLYBSTQVP4NB85V.code1.emi.philips.com>
2019-07-13 12:28:05 +0200

    2 new Guice test cases

--- java/guicetest/src/test/java/org/mahu/guicetest/GuiceProviderBindingTest.java
+++ java/guicetest/src/test/java/org/mahu/guicetest/GuiceProviderBindingTest.java
@@ -19,10 +19,14 @@ public class GuiceProviderBindingTest {
         @Override
         protected void configure() {
             bind(RequestData.class).toProvider(RequestDataProvider.class);
+            bind(ITestObject1.class).to(TestObject1.class);
         }
     }
 
     public static class RequestData {
+    	private static int globalSeqNr = 0;
+    	
+    	private int seqNr = globalSeqNr++;
     }
 
     static class RequestDataProvider implements Provider<RequestData> {
@@ -34,16 +38,38 @@ public class GuiceProviderBindingTest {
         }
 
     }
+    
+    static interface ITestObject1 {
+    	
+    	public RequestData getRequestData();
+    	
+    }
 
-    static class TestObject {
+    static class TestObject1 implements ITestObject1{
         final RequestData requestData;
 
         @Inject
-        TestObject(final RequestData requestData) {
+        TestObject1(final RequestData requestData) {
             this.requestData = requestData;
+        }
+        
+        public RequestData getRequestData() {
+        	return requestData;
         }
 
     }
+    
+    static class TestObject2 {
+        final RequestData requestData;
+        final ITestObject1 testObject1;
+
+        @Inject
+        TestObject2(final RequestData requestData, final ITestObject1 testObject1) {
+            this.requestData = requestData;
+            this.testObject1 = testObject1;
+        }
+
+    }    
 
     static interface IData {
 
@@ -79,11 +105,20 @@ public class GuiceProviderBindingTest {
     @Test
     public void requestScope() throws Exception {
         Injector injector = Guice.createInjector(new BindingModule());
-        TestObject testObject1 = injector.getInstance(TestObject.class);
-        TestObject testObject2 = injector.getInstance(TestObject.class);
+        TestObject1 testObject1 = injector.getInstance(TestObject1.class);
+        TestObject1 testObject2 = injector.getInstance(TestObject1.class);
 
         // Check that provider can be used to get 2 objects.
         assertNotEquals(testObject1.requestData, testObject2.requestData);
     }
+    
+    @Test
+    public void checkThatProviderIsCalledForEachInstance() throws Exception {
+        Injector injector = Guice.createInjector(new BindingModule());
+        TestObject2 testObject = injector.getInstance(TestObject2.class);
+
+        // Check that provider can be used to get 2 objects.
+        assertNotEquals(testObject.requestData.seqNr, testObject.testObject1.getRequestData().seqNr);
+    }    
 
 }
--- java/guicetest/src/test/java/org/mahu/guicetest/GuiceRequestScopeTest.java
+++ java/guicetest/src/test/java/org/mahu/guicetest/GuiceRequestScopeTest.java
@@ -24,15 +24,23 @@ import com.google.inject.servlet.ServletScopes;
 public class GuiceRequestScopeTest {
 
     static class BindingModule extends AbstractModule {
+    	    	
         @Override
         protected void configure() {
-            // Next line will result in creation of 1 RequestData object for a
-            // RequestScope.
-            bind(RequestData.class).in(ServletScopes.REQUEST);
+            // Next line will result in creation of 1 RequestData object for a RequestScope.
+        	// BUT, an object will be created only when there is none exists in the RequestScope map.
+        	// That map can be initialized with an object.
+            bind(IRequestData.class).to(RequestData.class).in(ServletScopes.REQUEST);
         }
-    }    
+    }
+    
+    interface IRequestData {
+    	
+    	long now();
+    	
+    }
 
-    public static class RequestData {
+    public static class RequestData implements IRequestData {
     	private static final long TWO_MS = 2;
 		final long now = System.currentTimeMillis();
     	
@@ -43,13 +51,18 @@ public class GuiceRequestScopeTest {
 				// ignore
 			}
     	}
+
+		@Override
+		public long now() {
+			return now;
+		}
     }
 
     static class TestObject {
-        final RequestData requestData;
+        final IRequestData requestData;
 
         @Inject
-        TestObject(final RequestData requestData) {
+        TestObject(final IRequestData requestData) {
             this.requestData = requestData;
         }
     }
@@ -86,6 +99,8 @@ public class GuiceRequestScopeTest {
 
     }
 
+    // Note that with an EMPTY seedMap, an object is created by Guice and added to the RequestScope map.
+    // That object is used thereafter.
     @Test
     public void requestScope_emptySeedMap_guiceCreatesOneObject() throws Exception {
         Injector injector = Guice.createInjector(new BindingModule());
@@ -98,7 +113,7 @@ public class GuiceRequestScopeTest {
                 assertNotNull(testObject1.requestData);
 
                 TestObject testObject2 = injector.getInstance(TestObject.class);
-                assertEquals(testObject1.requestData.now, testObject2.requestData.now);
+                assertEquals(testObject1.requestData.now(), testObject2.requestData.now());
 
                 return testObject1;
             }
@@ -114,14 +129,13 @@ public class GuiceRequestScopeTest {
     }
     
     @Test
-    public void requestScope_seedMapWithData_guiceUsesProviderInstance() throws Exception {
-        Injector injector = Guice.createInjector(new BindingModule());
+    public void requestScope_seedMapWithDataOutsideExecute_guiceUsesProviderInstance() throws Exception {       
         RequestData requestData = new RequestData();
         Map<Key<?>, Object> seedMap = new HashMap<>();
-        seedMap.put(Key.get(RequestData.class), requestData);
+        seedMap.put(Key.get(IRequestData.class), requestData);
         
         final ExecutorUsingRequestScope executorUsingRequestScope = new ExecutorUsingRequestScope(seedMap);
-
+        final Injector injector = Guice.createInjector(new BindingModule());        
         TestObject testObject1 = executorUsingRequestScope.execute(new RequestScopeRunnable<TestObject>() {
             public TestObject run() {
                 return injector.getInstance(TestObject.class);
@@ -130,7 +144,7 @@ public class GuiceRequestScopeTest {
         
         assertNotNull(testObject1);
         assertNotNull(testObject1.requestData);
-        assertEquals(requestData.now, testObject1.requestData.now);
+        assertEquals(requestData.now, testObject1.requestData.now());
         
         TestObject testObject2 = executorUsingRequestScope.execute(new RequestScopeRunnable<TestObject>() {
             public TestObject run() {
@@ -140,7 +154,37 @@ public class GuiceRequestScopeTest {
         
         assertNotNull(testObject2);
         assertNotNull(testObject2.requestData);
-        assertEquals(requestData.now, testObject2.requestData.now);        
-    }    
+        assertEquals(requestData.now, testObject2.requestData.now());        
+    }
+    
+    @Test
+    public void requestScope_seedMapWithDataInsideExecute_guiceUsesProviderInstance() throws Exception {
+        RequestData requestData = new RequestData();
+        Map<Key<?>, Object> seedMap = new HashMap<>();
+        seedMap.put(Key.get(IRequestData.class), requestData);
+        
+        final ExecutorUsingRequestScope executorUsingRequestScope = new ExecutorUsingRequestScope(seedMap);
+        TestObject testObject1 = executorUsingRequestScope.execute(new RequestScopeRunnable<TestObject>() {
+            public TestObject run() {
+                Injector injector = Guice.createInjector(new BindingModule());            	
+                return injector.getInstance(TestObject.class);
+            }
+        });
+        
+        assertNotNull(testObject1);
+        assertNotNull(testObject1.requestData);
+        assertEquals(requestData.now, testObject1.requestData.now());
+        
+        TestObject testObject2 = executorUsingRequestScope.execute(new RequestScopeRunnable<TestObject>() {
+            public TestObject run() {
+                Injector injector = Guice.createInjector(new BindingModule());              	
+                return injector.getInstance(TestObject.class);
+            }
+        });
+        
+        assertNotNull(testObject2);
+        assertNotNull(testObject2.requestData);
+        assertEquals(requestData.now(), testObject2.requestData.now());        
+    }     
 
 }
